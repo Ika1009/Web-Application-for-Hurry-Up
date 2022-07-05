b102ed72d369eb514deaa8cf4e2a3e87504dab4e
Ika1009 <ilija.doncic@yahoo.com>
2022-07-05 10:40:31 +0200

    Dodao funkcije da na klik na dugme login i signup se ode na index.html

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
-let ime;
-document.getElementById("dugme").onclick = function()
-{
-    ime = document.getElementById("textBox").value;
-    document.getElementById("paragraf").innerHTML = ime;
 
+document.getElementById("prijaviSeButton").onclick = function()
+{
+    window.location = 'http://localhost/index.html'
 }
