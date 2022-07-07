54f346994749ac7378d6b7ea52f8403a7d3b400b
Ika1009 <ilija.doncic@yahoo.com>
2022-07-07 20:28:39 +0200

    poboljasan popup al idalje ruzan :(

--- strane/artikli/artikli.js
+++ strane/artikli/artikli.js
@@ -1,5 +1,8 @@
 
-function plusButtonClick()
-{
-
+function login(showhide){
+    if(showhide == "show"){
+        document.getElementById('popupbox').style.visibility="visible";
+    }else if(showhide == "hide"){
+        document.getElementById('popupbox').style.visibility="hidden"; 
+    }
 }
