a08df60015357203675a92e7ffed466f559104dd
Ilija Dončić <75073537+Ika1009@users.noreply.github.com>
2022-07-09 12:02:03 +0200

    Update script.js
    imam dobar if() za menjanje brojanja ali ne znam dobar kod unutra

--- strane/ponuda/script.js
+++ strane/ponuda/script.js
@@ -1,4 +1,4 @@
-﻿let sveUkupno = 0;
+let sveUkupno = 0;
 
 function addToCart(element) {
 	let mainEl = element.closest('.single-item');
@@ -7,13 +7,29 @@ function addToCart(element) {
 	let kolicina = 1;
 	let cartItems = document.querySelector('.cart-items');
 
-	if (parseInt(kolicina) > 0) {
+	if(cartItems.textContent.includes(name)) // ovo je dobar if ali ne znam kod unutra kako treba da bude!
+	{
+		price = price.substring();
+		kolicina += 1;
+		let ukupno = parseInt(price) * parseInt(kolicina);
+		sveUkupno += ukupno;
+		cartItems.innerHTML +=  `<div class="cart-single-item">
+									<h3>${name}</h3>
+									<p id="name">${price} * ${kolicina} = <span>${ukupno} RSD</span></p>
+									<button onclick="removeFromCart(this)" class="remove-item">-</button>
+								 </div>`;
+		document.querySelector('.ispis').style.visibility = 'hidden';
+		document.querySelector('.total').style.visibility = 'visible';
+		document.querySelector('.total').innerText = `Naruči za: ${sveUkupno} RSD`;	
+	}
+
+	else if (parseInt(kolicina) > 0) {
 		price = price.substring(); 
 		let ukupno = parseInt(price) * parseInt(kolicina);
 		sveUkupno += ukupno;
 		cartItems.innerHTML +=  `<div class="cart-single-item">
 									<h3>${name}</h3>
-									<p>${price} * ${kolicina} = <span>${ukupno} RSD</span></p>
+									<p id="name">${price} * ${kolicina} = <span>${ukupno} RSD</span></p>
 									<button onclick="removeFromCart(this)" class="remove-item">-</button>
 								 </div>`;
 		document.querySelector('.ispis').style.visibility = 'hidden';
