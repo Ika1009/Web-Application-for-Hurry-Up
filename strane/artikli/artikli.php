3640fd1b8de10ab2179cdd013e117502e62e6be8
Ilija Dončić <75073537+Ika1009@users.noreply.github.com>
2022-07-12 18:44:15 +0200

    Update artikli.php
    dodao formu i div za nju da bi radilo za baze

--- strane/artikli/artikli.php
+++ strane/artikli/artikli.php
@@ -28,11 +28,13 @@
             <button type="dodaj" class="dugmeZaDodavanje" onclick="otvoriPopup()">Dodaj</button>
                 <div class="popup" id="popup">
                 <h3>Dodaj novi artikal:</h3>
+                <div id="signup-form">
                     <form name="form1" action="" method="post" enctype="multipart/form-data">
                         <input id="ime" type="text" name="ime" required placeholder="Ime Artikla" />
                         <input id="cena" type="text" name="cena" required placeholder="Cena" />
                         <input id="opis" type="text" name="opis" required placeholder="Opis" />
                     </form>
+                </div>
                 <button type="submit" name="submit" value="add"  onclick="ZatvoriPopUp()">Dodaj</button>
             </div>
         </div>
