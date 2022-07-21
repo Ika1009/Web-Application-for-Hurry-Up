b95dd2ff58558b8250d2b160eb316a321e1d60a4
Ika1009 <ilija.doncic@yahoo.com>
2022-07-21 15:52:09 +0200

    df

---
+++ strane/narudzbine/data.php
@@ -0,0 +1,22 @@
+<?php
+
+$servername = "localhost";  
+$username   = "hurryupr_milos";  
+$password   = "miloskralj";  
+$dbname     = "hurryupr_database1";  
+// Create connection  
+$conn = new mysqli($servername, $username, $password, $dbname);  
+// Check connection  
+if ($conn->connect_error) {  
+    die("Connection failed: " . $conn->connect_error);  
+} 
+$result = mysqli_query($conn, "SELECT * FROM `narudzbine`");
+$data = array();
+while ($row = mysqli_fetch_object($result))
+{
+    array_push($data, $row);
+}
+echo json_encode($data);
+exit();
+
+?>
