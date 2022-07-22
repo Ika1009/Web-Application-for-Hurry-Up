ddb386d42263fa63bb8454d6dcbe15c2a0a2b3f0
Milos Mitrovic <milosmitrovic005@gmail.com>
2022-07-22 13:30:30 +0200

    ispravke

--- index.php
+++ index.php
@@ -6,17 +6,13 @@
     <title>Hurry up</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
     <link href="prijava.css" rel="stylesheet" />
-    <style>
-        ul {
-            display: block; list-style-type: disc; margin-top: 0em; margin-bottom: 0em; margin-left: 0; margin-right: 0; padding-left: 40px; color: red;
-        }
-    </style>
+    <script src="script.js" defer></script>
 </head>
 <body>
     <div class="form-modal">
         <div class="form-toggle">
-            <button id="login-toggle" onclick="toggleLogin()">Prijavi se</button>
-            <button id="signup-toggle" onclick="toggleSignup()">Registruj se</button>
+            <button id="login-toggle" onclick="toggleSingup()">Prijavi se</button>
+            <button id="signup-toggle" onclick="toggleLogin()">Registruj se</button>
         </div>
         <div id="login-form">
             <form>
@@ -44,81 +40,5 @@
             </form>
         </div>
     </div>
-    <script src="script.js" async></script>
-    <script>
-    let unosi = document.querySelectorAll('input');
-    
-    let greske = {
-        "password": [],
-        "email": [],
-        "lozinka": [],
-        "pin": []
-    };
-    
-    unosi.forEach(element => {
-        element.addEventListener('change', e => {
-            let trenutniUnos = e.target;
-            let vrednostUnosa = trenutniUnos.value;
-            let imeUnosa = trenutniUnos.getAttribute('name');
-    
-            greske[imeUnosa] = [];
-    
-            switch (imeUnosa) {
-                case 'email':
-                    if (!proveraEmail(vrednostUnosa)) {
-                        greske[imeUnosa].push('Neispravna email adresa');
-                    }
-                    break;
-    
-                case 'lozinka':
-                    if (vrednostUnosa.length < 8) {
-                        greske[imeUnosa].push('Lozinka mora biti duža od 8 karaktera');
-                    }
-                    break;
-                    
-                case 'password':
-                    if (vrednostUnosa.length < 8) {
-                        greske[imeUnosa].push('Lozinka mora biti duža od 8 karaktera');
-                    }
-                    break;
-                
-                case 'pin': 
-                    if (vrednostUnosa.length !== 4) {
-                        greske[imeUnosa].push('Pin mora biti četvorocifren');
-                    }
-                    break;
-            }
-            
-            ceste_greske();
-        });
-    });
-    
-    const ceste_greske = () => {
-        for (let elem of document.querySelectorAll('ul')) {
-            elem.remove();
-        }
-    
-        for (let key of Object.keys(greske)) {
-            let unos = document.querySelector(`input[name = "${key}"]`);
-            let parentElement = unos.parentElement;
-            let greskeElement = document.createElement('ul');
-            parentElement.appendChild(greskeElement);
-    
-            greske[key].forEach(x => {
-                let li = document.createElement('li');
-                li.innerText = x;
-                greskeElement.appendChild(li);
-            });
-        }
-    }
-    
-    const proveraEmail = email => {
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-            return true;
-        }
-    
-        return false;
-    }
-    </script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -86,3 +86,77 @@ let session = getCookie();
 if (session !== "") {
     window.location = "http://www.hurryup.rs/dashboard";
 }
+
+let unosi = document.querySelectorAll('input');
+    
+let greske = {
+    "password": [],
+    "email": [],
+    "lozinka": [],
+    "pin": []
+};
+    
+unosi.forEach(element => {
+    element.addEventListener('change', e => {
+        let trenutniUnos = e.target;
+        let vrednostUnosa = trenutniUnos.value;
+        let imeUnosa = trenutniUnos.getAttribute('name');
+    
+        greske[imeUnosa] = [];
+    
+        switch (imeUnosa) {
+            case 'email':
+                if (!proveraEmail(vrednostUnosa)) {
+                    greske[imeUnosa].push('Neispravna email adresa');
+                }
+                break;
+    
+            case 'lozinka':
+                if (vrednostUnosa.length < 8) {
+                    greske[imeUnosa].push('Lozinka mora biti duža od 8 karaktera');
+                }
+                break;
+                    
+            case 'password':
+                if (vrednostUnosa.length < 8) {
+                    greske[imeUnosa].push('Lozinka mora biti duža od 8 karaktera');
+                }
+                break;
+                
+            case 'pin': 
+                if (vrednostUnosa.length !== 4) {
+                    greske[imeUnosa].push('Pin mora biti četvorocifren');
+                }
+                break;
+        }
+            
+        ceste_greske();
+    });
+});
+    
+const ceste_greske = () => {
+    for (let elem of document.querySelectorAll('ul')) {
+        elem.remove();
+    }
+    
+    for (let key of Object.keys(greske)) {
+        let unos = document.querySelector(`input[name = "${key}"]`);
+        let parentElement = unos.parentElement;
+        let greskeElement = document.createElement('ul');
+        parentElement.appendChild(greskeElement);
+    
+        greske[key].forEach(x => {
+            let li = document.createElement('li');
+            li.innerText = x;
+            greskeElement.appendChild(li);
+        });
+    }
+}
+    
+const proveraEmail = email => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return true;
+    }
+    
+    return false;
+}
