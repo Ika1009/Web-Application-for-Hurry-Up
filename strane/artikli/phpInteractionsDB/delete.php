97f0ea886e4d62b299f7ac742ed0b66ed560fcd1
Ika1009 <ilija.doncic@yahoo.com>
2022-07-24 12:57:53 +0200

    nadam se da radi ubacivanje kategorija

--- strane/artikli/phpInteractionsDB/delete.php
+++ strane/artikli/phpInteractionsDB/delete.php
@@ -1,30 +1,32 @@
 <?php
 
-            //za brisanje slike iz file system
-            $mask = 'artikliSlike/'.$_GET['id'].'.*';
-            array_map('unlink', glob($mask));
+//za brisanje slike iz file system
+$mask = 'artikliSlike/' . $_GET['id'] . '.*';
+array_map('unlink', glob($mask));
 
-            //brisanje iz baze
-            $servername = "localhost";  
-            $username   = "root";  
-            $password   = "";  
-            $dbname     = "hurryupr_database1"; 
-            // Create connection  
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection  
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
+//brisanje iz baze
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hurryupr_database1";
+// Create connection  
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection  
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-            $sql = "DELETE FROM artikli WHERE id = ".$_GET['id']; //kako sad ja da dobijem bas artikl koji se brise - evo kako ilija smrdljo
+$sql = "DELETE FROM artikli WHERE id = " . $_GET['id']; //kako sad ja da dobijem bas artikl koji se brise - evo kako ilija smrdljo
 
-            if ($conn->query($sql) === TRUE) {
-                $row_successfully_deleted = true;
-            } else {
-                echo "Error deleting record: " . $conn->error;
-            }
-            $conn->close();
+if ($conn->query($sql) === TRUE) {
+    $row_successfully_deleted = true;
+}
+else {
+    echo "Error deleting record: " . $conn->error;
+}
+$conn->close();
 
-            if($row_successfully_deleted) echo "deleted";
+if ($row_successfully_deleted)
+    echo "deleted";
 
-            ?>
+?>
