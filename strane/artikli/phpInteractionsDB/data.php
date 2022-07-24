05ff9ea7cd2d4eab814fdaf38dbaf97c1f01ae9b
Milos Mitrovic <milosmitrovic005@gmail.com>
2022-07-24 14:17:39 +0200

    ddvf

--- strane/artikli/phpInteractionsDB/data.php
+++ strane/artikli/phpInteractionsDB/data.php
@@ -3,7 +3,7 @@
 $servername = "localhost";  
 $username   = "root";  
 $password   = "";  
-$dbname     = "hurryupr_database1";  
+$dbname     = "milos";  
 // Create connection  
 $conn = new mysqli($servername, $username, $password, $dbname);  
 // Check connection  
