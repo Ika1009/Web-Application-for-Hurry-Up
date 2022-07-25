29590e230a099dc8effd3e84bec443921fbf7bd4
Ika1009 <ilija.doncic@yahoo.com>
2022-07-25 16:42:06 +0200

    evo je

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
 
-$servername = "localhost";
+$servername = "87.250.63.45";
 $username = "root";
-$password = "";
+$password = "gtrdsnetrt";
 $dbname = "hurryupr_database1";
+$port = "11883";
 // Create connection  
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 // Check connection  
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
