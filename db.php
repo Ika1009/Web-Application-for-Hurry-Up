bb7457579f00a4f1b59c4a4b6b2557f18b85d35a
Ika1009 <ilija.doncic@yahoo.com>
2022-07-25 16:48:59 +0200

    zajednicka baza provera

--- db.php
+++ db.php
@@ -4,12 +4,18 @@ $servername = "87.250.63.45";
 $username = "root";
 $password = "gtrdsnetrt";
 $dbname = "hurryupr_database1";
-$port = "11883";
+$charset = 'utf8mb4';
+$port = 11883;
 // Create connection  
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn->set_charset($charset);
+$conn->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
 // Check connection  
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+unset($servername, $dbname, $username, $password, $charset, $port); // we don't need them anymore
+
 ?>
+
