492d46fe03b2d04207a8fc5690dd816361401ddf
Ika1009 <ilija.doncic@yahoo.com>
2022-07-26 12:52:33 +0200

    popravljena ponuda

--- strane/narudzbine/APIs/updateDenied.php
+++ strane/narudzbine/APIs/updateDenied.php
@@ -1,1 +1,16 @@
+<?php
 
+include_once("../../../db.php");
+
+$sql = "UPDATE `artikli` SET `ime`=\"$ime\", `cena`=\"$cena\", `opis`=\"$opis\", `popust`=\"$popust\", `kategorija`=\"$kategorija\", `slika`=\"$slikaEkstenzija\" WHERE `id`= " . $_REQUEST['id'];
+
+if ($conn->query($sql) === TRUE) {
+    $row_successfully_updated = true;
+}
+else {
+    echo "Error deleting record: " . $conn->error;
+}
+$conn->close();
+
+if ($row_successfully_updated)
+    echo "deleted";
