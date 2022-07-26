505f6890aef2b58ff17e30cba5721e2fa2705f64
Ika1009 <ilija.doncic@yahoo.com>
2022-07-26 10:55:28 +0200

    includovao na jos neka mesta

--- registracija.php
+++ registracija.php
@@ -1,16 +1,7 @@
 <?php
 if (isset($_POST['register'])) {  
     extract($_POST);  
-    $servername = "localhost";  
-    $username   = "root";  
-    $password   = "";  
-    $dbname     = "milos";  
-    // Create connection  
-    $conn = new mysqli($servername, $username, $password, $dbname);  
-    // Check connection  
-    if ($conn->connect_error) {  
-        die("Connection failed: " . $conn->connect_error);  
-    }
+    include_once("db.php");
     $lozinka = password_hash($lozinka, PASSWORD_DEFAULT);
     $datum_kreiranja = $_COOKIE['createdAt'];
     $sql = "INSERT INTO `registracija` (ime, prezime, ime_firme, email, broj_telefona, lozinka, pin, datum_kreiranja) VALUES ('$ime','$prezime','$ime_firme','$email','$broj_telefona','$lozinka','$pin','$datum_kreiranja')";
