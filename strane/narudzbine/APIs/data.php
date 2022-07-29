caadbd3ff60c59fdcf6cbb7694223a025686734a
Ika1009 <ilija.doncic@yahoo.com>
2022-07-29 16:28:24 +0200

    popravljeno

--- strane/narudzbine/APIs/data.php
+++ strane/narudzbine/APIs/data.php
@@ -3,6 +3,7 @@
 session_start();
 include_once("../../../db.php");
 
+$ime_firme = $_SESSION['ime_firme'];
 $result = mysqli_query($conn, "SELECT * FROM `narudzbine` WHERE ime_firme = '$ime_firme'");
 $data = array();
 while ($row = mysqli_fetch_object($result))
