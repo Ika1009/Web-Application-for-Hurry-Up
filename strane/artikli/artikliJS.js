0dc529a5e190753f4b9197c2ba9beb45fcef3528
Ika1009 <ilija.doncic@yahoo.com>
2022-08-09 22:00:09 +0200

    alo alo zove vienna

--- strane/artikli/artikliJS.js
+++ strane/artikli/artikliJS.js
@@ -59,3 +59,19 @@ ajax1.onreadystatechange = function () {
         }
     }
 };
+
+let ajax1 = new XMLHttpRequest();
+ajax1.open("GET", "./API/kategorijeDobivanje.php", true);
+ajax1.send();
+ajax1.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let data = JSON.parse(this.responseText);
+    let html = "";
+    html += "<span class=svi onclick=kategorije(this)>Svi</span>";
+    for (let i = 0; i < data.length; i++) {
+      let kategorija = data[i].ime_kategorije;
+      html += "<span class=jednakat onclick=kategorije(this)>" + kategorija + "</span>";
+    }
+    document.getElementById("category").innerHTML += html;
+  }
+};
