284d02f4cca8e98d0f635070e991df2c83e8f376
Milos Mitrovic <milosmitrovic005@gmail.com>
2022-08-09 19:48:33 +0200

    ostala jos 2 dugmica da se povezu

--- strane/nalog/profile.php
+++ strane/nalog/profile.php
@@ -1,76 +1,69 @@
 <?php
 session_start();
-include('../../db.php');
-
-if (isset($_SESSION['userpin'])) {
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="profile.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
-        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-        <title>Moj nalog</title>
-        <link href="../../slike/hurryup_logo2.ico" rel="icon">
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="profile.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <title>Moj nalog</title>
+    <link href="../../slike/hurryup_logo2.ico" rel="icon">
+</head>
 
-    <body>
-        <header class="header">
-            <div class="container">
-                <div class="row align-items-center justify-content-between">
-                    <div class="logo">
-                        <img src="../../slike/hurryup_logo2.jpg" width="auto" height="57px">
-                    </div>
-                    <button type="button" class="nav-toggler">
-                        <span></span>
-                    </button>
-                    <nav class="nav">
-                        <ul>
-                            <li><a href="../narudzbine/narudzbine.php">Narudzbine</a></li>
-                            <li><a href="../artikli/artikli.php">Artikli</a></li>
-                            <li><a href="../ponuda/ponuda.php">Ponuda</a></li>
-                            <li><a class="active" href="../nalog/profile.php">Moj nalog</a></li>
-                        </ul>
-                    </nav>
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="logo">
+                    <img src="../../slike/hurryup_logo2.jpg" width="auto" height="57px">
                 </div>
+                <button type="button" class="nav-toggler">
+                    <span></span>
+                </button>
+                <nav class="nav">
+                    <ul>
+                        <li><a href="../narudzbine/narudzbine.php">Narudzbine</a></li>
+                        <li><a href="../artikli/artikli.php">Artikli</a></li>
+                        <li><a href="../ponuda/ponuda.php">Ponuda</a></li>
+                        <li><a class="active" href="../nalog/profile.php">Moj nalog</a></li>
+                    </ul>
+                </nav>
             </div>
-        </header>
-
-        <div class="d-flex align-items-center flex-column" style="min-height: 100vh;">
-            <i class="bi bi-person-fill" style="font-size: 14rem"></i>
-            <h1 class="text-center display-4" style="margin-top: -10px; font-size: 2rem;"><?= $_SESSION['ime_firme'] ?></h1>
-            <a href="../../prijava/logout.php" class="btn btn-info">Izmeni informacije</a><br>
-            <a href="../../prijava/logout.php" class="btn btn-warning">Promeni lozinku</a><br>
-            <a href="../../prijava/logout.php" class="btn btn-warning">Odjava</a>
         </div>
+    </header>
 
-        <script>
-            const navToggler = document.querySelector(".nav-toggler");
-            navToggler.addEventListener("click", navToggle);
+    <div class="p-5 rounded shadow custom-justify-content align-items-center d-flex flex-column" style="width: 60%; margin: auto; padding: 10px;">
+        <i class="bi bi-person-fill" style="font-size: 14rem"></i>
+        <h1 class="text-center display-4" style="margin-top: -10px; font-size: 2rem;"><?= $_SESSION['ime_firme'] ?></h1>
+        <a href="#" class="btn btn-info">Izmeni informacije</a>
+        <a href="#" class="btn btn-change">Promeni lozinku</a>
+        <a href="../../prijava/logout.php" class="btn btn-warning">Odjava</a>
+    </div>
 
-            function navToggle() {
-                navToggler.classList.toggle("active");
-                const nav = document.querySelector(".nav");
-                nav.classList.toggle("open");
-                if (nav.classList.contains("open")) {
-                    nav.style.maxHeight = nav.scrollHeight + "px";
-                } else {
-                    nav.removeAttribute("style");
-                }
+    <script>
+        const navToggler = document.querySelector(".nav-toggler");
+        navToggler.addEventListener("click", navToggle);
+
+        function navToggle() {
+            navToggler.classList.toggle("active");
+            const nav = document.querySelector(".nav");
+            nav.classList.toggle("open");
+            if (nav.classList.contains("open")) {
+                nav.style.maxHeight = nav.scrollHeight + "px";
+            } else {
+                nav.removeAttribute("style");
             }
-        </script>
-    </body>
+        }
+    </script>
+</body>
 
-    </html>
-<?php
-} else {
-    header('Location: login.php');
-}
+</html>
