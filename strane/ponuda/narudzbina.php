10d107824517199eac14a97bfc71dd0a66ef0f64
Ika1009 <ilija.doncic@yahoo.com>
2022-08-09 20:07:10 +0200

    rade qr kodovi

--- strane/ponuda/narudzbina.php
+++ strane/ponuda/narudzbina.php
@@ -1,14 +1,18 @@
 <?php
-session_start();
+
 if (isset($_POST['order'])) { 
     include_once("../../db.php");
+    echo "UPAD LELELELE";
+    $broj_stola = $_COOKIE['broj_stola'];
+    echo $broj_stola;
     $vreme_narucivanja = $_COOKIE['vreme_narucivanja'];
     $konacna_cena = $_COOKIE['narudzbina'];
     $detalji = $_COOKIE['detalji'];
-    $ime_firme = $_SESSION['ime_firme'];
-    $sql = "INSERT INTO `narudzbine` (ukupna_cena, vreme_narucivanja, broj_stola, ime_firme, detalji, napomena) VALUES ('$konacna_cena', '$vreme_narucivanja', 'qr kod', '$ime_firme', '$detalji', 'nema')";
+    $ime_firme = $_COOKIE['ime_firme'];
+    echo $ime_firme;
+    $sql = "INSERT INTO `narudzbine` (ukupna_cena, vreme_narucivanja, broj_stola, ime_firme, detalji, napomena) VALUES ('$konacna_cena', '$vreme_narucivanja', '$broj_stola', '$ime_firme', '$detalji', 'nema')";
     if ($conn->query($sql) === TRUE) {
-        header('Location: ponuda.php');  
+        header('Location: ponuda.php'); // treba trenutna lokacija, jer qr kodovi nisu samo ponuda.php
     } else {  
         echo "Greska: " . $sql . "<br>" . $conn->error;  
     }
