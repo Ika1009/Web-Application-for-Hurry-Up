2e63056e2a0a9b951034df4cfa6098e417aaf1d3
Ika1009 <ilija.doncic@yahoo.com>
2022-08-11 20:14:08 +0200

    radi ispisvanje artikala koji nisu na stanju

--- strane/artikli/artikliJS.js
+++ strane/artikli/artikliJS.js
@@ -14,27 +14,56 @@ ajax.onreadystatechange = function () {
             let opis = data[i].opis;
             let popust = data[i].popust;
             let kategorija = data[i].kategorija;
-            html += "<div class=product>";
-            html += "<input class=\"id_artikla\" data-id=\"" + id + "\" type=\"hidden\">";
-            html += "<div class=divdugizlaz>";
-            html += "<img src=artikliSlike/" + id + "." + slika + ">";
-            html += "<ion-icon class=dugizlaz name=close-outline onclick=onClickDugmeZaBrisanje(this)>Edit</ion-icon>";
-            html += "<ion-icon class=dugedit name=pencil onclick=dugmeZaMenjanje(this)></ion-icon><br><br>";
-            html += "</div>";
-            html += "<div class=imecenakat>"
-            html += "<h3>" + ime + "</h3>";
-            html += "<p class=cat>" + kategorija + "</p>";
-            if (popust != '0') {
-                html += "<div class=divcena>"
-                html += "<div class=price>" + cena * (100 - parseInt(popust)) / 100 + " RSD</div>";
-                html += "<div class=priceprecrtano>" + cena + " RSD</div>"; // precrtaj
+            let na_stanju = data[i].na_stanju;
+            if (na_stanju) {
+                // console.log("jeste na stanju")
+                html += "<div class=product>";
+                html += "<input class=\"id_artikla\" data-id=\"" + id + "\" type=\"hidden\">";
+                html += "<div class=divdugizlaz>";
+                html += "<img src=artikliSlike/" + id + "." + slika + ">";
+                html += "<ion-icon class=dugizlaz name=close-outline onclick=onClickDugmeZaBrisanje(this)>Edit</ion-icon>";
+                html += "<ion-icon class=dugedit name=pencil onclick=dugmeZaMenjanje(this)></ion-icon><br><br>";
+                html += "</div>";
+                html += "<div class=imecenakat>"
+                html += "<h3>" + ime + "</h3>";
+                html += "<p class=cat>" + kategorija + "</p>";
+                if (popust != '0') {
+                    html += "<div class=divcena>"
+                    html += "<div class=price>" + cena * (100 - parseInt(popust)) / 100 + " RSD</div>";
+                    html += "<div class=priceprecrtano>" + cena + " RSD</div>"; // precrtaj
+                    html += "</div>"
+                } else {
+                    html += "<div class=price>" + cena + " RSD</div>";
+                }
+                html += "<p class=desc>" + opis + "</p>";
                 html += "</div>"
+                html += "</div>";
             } else {
-                html += "<div class=price>" + cena + " RSD</div>";
+                // console.log("nije na stanju")
+                html += "<div class=product>";
+                html += "<input class=\"id_artikla\" data-id=\"" + id + "\" type=\"hidden\">";
+                html += "<div class=divdugizlaz>";
+                html += "<img src=artikliSlike/" + id + "." + slika + ">";
+                html += "<ion-icon class=dugizlaz name=close-outline onclick=onClickDugmeZaBrisanje(this)>Edit</ion-icon>";
+                html += "<ion-icon class=dugedit name=pencil onclick=dugmeZaMenjanje(this)></ion-icon><br><br>";
+                html += "</div>";
+                html += "<div class=imecenakat>"
+                html += "<h3>" + ime + "</h3>";
+                html += "<h2> NIJE NA STANJU <h2>";
+                html += "<p class=cat>" + kategorija + "</p>";
+                if (popust != '0') {
+                    html += "<div class=divcena>"
+                    html += "<div class=price>" + cena * (100 - parseInt(popust)) / 100 + " RSD</div>";
+                    html += "<div class=priceprecrtano>" + cena + " RSD</div>"; // precrtaj
+                    html += "</div>"
+                } else {
+                    html += "<div class=price>" + cena + " RSD</div>";
+                }
+                html += "<p class=desc>" + opis + "</p>";
+                html += "</div>"
+                html += "</div>";
             }
-            html += "<p class=desc>" + opis + "</p>";
-            html += "</div>"
-            html += "</div>";
+
         }
         document.getElementById("data").innerHTML += html;
     }
