5fa9a5041af6393a7f86cf2e827682854516a058
Milos Mitrovic <milosmitrovic005@gmail.com>
2022-08-11 10:04:32 +0200

    ponuda za goste

---
+++ API/data.php
@@ -0,0 +1,12 @@
+<?php
+include_once("../db.php");
+
+$ime_firme = $_REQUEST['ime_firme'];
+$result = mysqli_query($conn, "SELECT * FROM `artikli` WHERE ime_firme = '$ime_firme'");
+$data = array();
+while ($row = mysqli_fetch_object($result))
+{
+    array_push($data, $row);
+}
+echo json_encode($data);
+exit();
